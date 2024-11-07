452bee81f3aa40414b5c029f3b5a3aa6a719da8a
Densodim <25denso@gmail.com>
2024-11-07 08:28:45 +0300

    create the header and image section of the post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,12 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 import {Post} from "@/components/Posts";
+import {AdjustmentsHorizontalIcon} from '@heroicons/react/24/solid'
 
 export default function Post({id, img, userImg, username, caption}:Post) {
     return (
         <>
-            <div>
-                <h1>{username}</h1>
+            <div className='bg-white my-7 border rounded-md'>
+            {/*    Post Header*/}
+                <div className='flex items-center p-5'>
+                    <img src={userImg} alt={caption} className='h-12 rounded-full object-cover border p-1 mr-3'/>
+                    <p className="font-bold flex-1">{username}</p>
+                    <AdjustmentsHorizontalIcon className='h-5'/>
+                </div>
+
+            {/*    Post Image*/}
+                <img src={img} alt='' className='object-cover w-full'/>
             </div>
         </>
     )
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -5,14 +5,14 @@ export default function Posts() {
         {
             id: '1',
             username: 'John',
-            userImg: 'https://celes.club/collections/uploads/posts/2024-02/1709038010_celes-club-p-znachok-punkta-instagram-69.png',
+            userImg: 'https://mobile-review.com/all/wp-content/uploads/2021/07/instagram-logo.png',
             img: 'https://images.unsplash.com/photo-1730829807497-9c5b8c9c41c4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
             caption: 'Some text here'
         },
         {
             id: '2',
             username: 'John2',
-            userImg: 'https://celes.club/collections/uploads/posts/2024-02/1709038010_celes-club-p-znachok-punkta-instagram-69.png',
+            userImg: 'https://mobile-review.com/all/wp-content/uploads/2021/07/instagram-logo.png',
             img: 'https://images.unsplash.com/photo-1730774487035-05673e0c5747?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
             caption: 'Some text here'
         }
