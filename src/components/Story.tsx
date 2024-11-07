fd68864d28df6c2c3490cbf8329debefb773fb9b
Densodim <25denso@gmail.com>
2024-11-07 07:52:22 +0300

    stlw the story sextion and install tailwind scrollbar

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,9 +1,13 @@
-export default function Story({img, username}:Props) {
+export default function Story({img, username}: Props) {
 
     return (
         <>
-            <img src={img} alt={username} />
-            <p>{username}</p>
+            <div>
+                <img src={img} alt={username}
+                     className='h-14 rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out'/>
+                <p className="text-xs w-14 truncate">{username}</p>
+            </div>
+
         </>
     )
 }
