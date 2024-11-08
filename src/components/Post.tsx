212c0cae44cbfc58dc82ac74c7b7849aef7341ef
Densodim <25denso@gmail.com>
2024-11-08 06:59:10 +0300

    create the buttons sextion of the post components

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,21 +1,36 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 import {Post} from "@/components/Posts";
-import {AdjustmentsHorizontalIcon} from '@heroicons/react/24/solid'
+import {
+    BookmarkIcon,
+    ChatBubbleBottomCenterTextIcon,
+    EllipsisHorizontalIcon,
+    HeartIcon
+} from '@heroicons/react/24/solid'
 
-export default function Post({id, img, userImg, username, caption}:Post) {
+
+export default function Post({id, img, userImg, username, caption}: Post) {
     return (
         <>
             <div className='bg-white my-7 border rounded-md'>
-            {/*    Post Header*/}
+                {/*    Post Header*/}
                 <div className='flex items-center p-5'>
                     <img src={userImg} alt={caption} className='h-12 rounded-full object-cover border p-1 mr-3'/>
                     <p className="font-bold flex-1">{username}</p>
-                    <AdjustmentsHorizontalIcon className='h-5'/>
+                    <EllipsisHorizontalIcon className='h-5'/>
                 </div>
 
-            {/*    Post Image*/}
+                {/*    Post Image*/}
                 <img src={img} alt='' className='object-cover w-full'/>
+
+                {/*   Post  Buttons */}
+                <div className='flex justify-between px-4 pt-4'>
+                    <div className='flex space-x-4'>
+                        <HeartIcon className='btn'/>
+                        <ChatBubbleBottomCenterTextIcon className='btn'/>
+                    </div>
+                    <BookmarkIcon className='btn'/>
+                </div>
             </div>
         </>
     )
