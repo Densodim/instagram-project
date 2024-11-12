4d31c5545a7e1877dae3e1a5fc317dd86ddd0f4d
Densodim <25denso@gmail.com>
2024-11-12 08:10:49 +0300

    Stories components

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,7 +1,8 @@
+
 "use client"
 
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
 import minifaker from "minifaker"
 import "minifaker/locales/en"
 import Story from "@/components/Story"
